66762d3658f294af79ebdd144563f87f88ef60b2
Clay Chang <clay.chang@gmail.com>
2021-04-09 08:52:05 +0800

    add nokaslr to append; add -s to qemu

--- start_qemu.sh
+++ start_qemu.sh
@@ -2,7 +2,7 @@
 
 qemu/x86_64-softmmu/qemu-system-x86_64 \
     -kernel linux/arch/x86/boot/bzImage \
-    -append "console=ttyS0" \
+    -append "console=ttyS0 nokaslr" \
     -initrd rootfs/initramfs.cpio.gz \
     -blockdev driver=file,filename=edk2/Build/Ovmf3264/DEBUG_GCC5/FV/OVMF_CODE.fd,node-name=libvirt-pflash0-storage,auto-read-only=on,discard=unmap \
     -blockdev node-name=libvirt-pflash0-format,read-only=on,driver=raw,file=libvirt-pflash0-storage \
@@ -12,4 +12,5 @@ qemu/x86_64-softmmu/qemu-system-x86_64 \
     -nographic \
     -cpu Icelake-Server \
     -machine pc-q35-5.0,pflash0=libvirt-pflash0-format,pflash1=libvirt-pflash1-format \
-    -enable-kvm
+    -enable-kvm \
+    -s
