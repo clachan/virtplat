d1ed57ad3264c377681fe8994a48f1a76c73099b
Clay Chang <clay.chang@gmail.com>
2021-04-12 00:13:36 +0800

    revise debug_linux.sh

--- debug_linux.sh
+++ debug_linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cat << EOF > /tmp/.gdb.linux.init
-target remote :1234
+set auto-load safe-path /
+set directories linux
 file linux/vmlinux
 source linux/vmlinux-gdb.py
+target remote :1234
 EOF
 
 cgdb -x /tmp/.gdb.linux.init
