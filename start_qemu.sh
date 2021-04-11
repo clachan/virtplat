5ab1d5ade54f344a40992d0c04620f860894f94d
Clay Chang <clay.chang@gmail.com>
2021-04-11 15:13:54 +0800

    reorder parameters for pretty

--- start_qemu.sh
+++ start_qemu.sh
@@ -8,10 +8,10 @@ qemu/x86_64-softmmu/qemu-system-x86_64 \
     -blockdev node-name=libvirt-pflash0-format,read-only=on,driver=raw,file=libvirt-pflash0-storage \
     -blockdev driver=file,filename=edk2/Build/Ovmf3264/DEBUG_GCC5/FV/OVMF_VARS.fd,node-name=libvirt-pflash1-storage,auto-read-only=on,discard=unmap \
     -blockdev node-name=libvirt-pflash1-format,read-only=on,driver=raw,file=libvirt-pflash1-storage \
-    -netdev user,id=user.0 -device virtio-net-pci,netdev=user.0 \
-    -m 16384M \
-    -nographic \
+    -machine pc-q35-5.0,smm=on,pflash0=libvirt-pflash0-format,pflash1=libvirt-pflash1-format \
     -cpu Icelake-Server \
-    -machine pc-q35-5.0,pflash0=libvirt-pflash0-format,pflash1=libvirt-pflash1-format \
+    -m 16384M \
+    -netdev user,id=user.0 -device virtio-net-pci,netdev=user.0 \
     -enable-kvm \
+    -nographic \
     -s
