1c553e8858e83de3a2ed465812870aa77047b6e8
Clay Chang <clay.chang@gmail.com>
2021-04-11 21:39:54 +0800

    add argc check in start_qemu.sh
    To start with minimal rootfs, run:
    
    ./start_qemu.sh
    
    To start with ubuntu, run:
    
    ./start_qemu.sh virtplat_rootfs=ubuntu    to start with ubuntu

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [ $# -gt 0 ]
+then
+  VIRTPLAT_ROOTFS="virtplat_rootfs=ubuntu"
+  VIRTPLAT_ROOTFS_DRIVE="-drive file=rootfs/ubuntu-20.04-server-cloudimg-amd64.img"
+else
+  VIRTPLAT_ROOTFS=""
+  VIRTPLAT_ROOTFS_DRIVE=""
+fi
+
 qemu/x86_64-softmmu/qemu-system-x86_64 \
     -kernel linux/arch/x86/boot/bzImage \
-    -append "console=ttyS0 nokaslr" \
+    -append "console=ttyS0 nokaslr $VIRTPLAT_ROOTFS" \
     -initrd rootfs/initramfs.cpio.gz \
     -blockdev driver=file,filename=edk2/Build/Ovmf3264/DEBUG_GCC5/FV/OVMF_CODE.fd,node-name=libvirt-pflash0-storage,auto-read-only=on,discard=unmap \
     -blockdev node-name=libvirt-pflash0-format,read-only=on,driver=raw,file=libvirt-pflash0-storage \
@@ -11,6 +20,7 @@ qemu/x86_64-softmmu/qemu-system-x86_64 \
     -machine pc-q35-5.0,smm=on,pflash0=libvirt-pflash0-format,pflash1=libvirt-pflash1-format \
     -cpu Icelake-Server \
     -m 16384M \
+    $VIRTPLAT_ROOTFS_DRIVE \
     -netdev user,id=user.0 -device virtio-net-pci,netdev=user.0 \
     -enable-kvm \
     -nographic \
