aa7996632c654491bcb91a088d97458a4b3cc921
Clay Chang <clay.chang@gmail.com>
2021-04-12 10:23:46 +0800

    add 'set auto-load safe-path /' to .gdb.init

--- debug_linux.sh
+++ debug_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat << EOF > /tmp/.gdbinit.linux
-set auto-load-safe-path linux/scripts/gdb/vmlinux-gdb.py
+set auto-load safe-path /
 set directories linux
 file linux/vmlinux
 source linux/vmlinux-gdb.py
