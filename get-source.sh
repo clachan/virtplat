050044a04b4c2a76eb520bd502b06537587f6b2c
Clay Chang <clay.chang@gmail.com>
2021-05-15 17:53:28 +0800

    revise rootfs path

--- get-source.sh
+++ get-source.sh
@@ -13,6 +13,7 @@ git clone -b v5.12 --single-branch --depth 1 https://github.com/torvalds/linux.g
 
 git clone -b 1_33_stable --single-branch --depth 1 https://github.com/mirror/busybox.git
 
+cd ..
 mkdir -p rootfs
 wget https://cloud-images.ubuntu.com/releases/hirsute/release/ubuntu-21.04-server-cloudimg-amd64.img \
   -O rootfs/ubuntu-21.04-server-cloudimg-amd64.img
